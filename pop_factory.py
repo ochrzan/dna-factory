0f82ae1d53e1ad6274f89cf417412042b9b8d2d5
Oliver Chrzan <ochrzan@cargurus.com>
2020-01-15 14:20:50 -0500

    Adding in code to configure pathogens. Some tests for PathogenGroup

--- pop_factory.py
+++ pop_factory.py
@@ -9,6 +9,7 @@ Similar to http://cnsgenomics.com/software/gcta/#GWASSimulation ?
 """
 
 import getopt
+import random
 import sys
 import glob
 import yaml
@@ -32,6 +33,7 @@ class SNPTuples:
     def __init__(self, snp_id):
         self.id = snp_id
         self.tuples = []
+        self.minor_tuple = None
 
     def add_tuple(self, inserted, range_end):
         self.tuples.append((inserted, range_end))
@@ -46,28 +48,37 @@ class SNPTuples:
         Returns the second most probable snp value as the "pathogen" mutation
         :return: second most probable nucleotide for this snp
         """
-        if len(self.tuples) == 1:
-            return self.tuples[0][0]
-        prev_prob = 0
-        sorted_probabilities = []
-        for nt_letter, prob in self.tuples:
-            sorted_probabilities.append((nt_letter, prob - prev_prob))
-            prev_prob = prob
-        sorted_probabilities.sort(key=lambda x: x[1], reverse=True)
-        return sorted_probabilities[1][0]
+
+        return self.minor_allele_tuple()[0]
+
+    def minor_allele_tuple(self):
+        """
+        Returns the second most probable snp and it's frequency
+        :return: second most probable nucleotide for this snp and it's frequency
+        """
+        if self.minor_tuple is None:
+            if len(self.tuples) == 1:
+                self.minor_tuple = self.tuples[0]
+            else:
+                prev_prob = 0
+                sorted_probabilities = []
+                for nt_letter, prob in self.tuples:
+                    sorted_probabilities.append((nt_letter, prob - prev_prob))
+                    prev_prob = prob
+                sorted_probabilities.sort(key=lambda x: x[1], reverse=True)
+                self.minor_tuple = sorted_probabilities[1]
+        return self.minor_tuple
 
 class PopulationFactory:
 
     # number of subgroups with phenotype, total number of hidden mutations
-    def __init__(self, num_groups, num_mutations):
-        self.num_groups = num_groups
-        self.num_mutations = num_mutations
+    def __init__(self):
         self.pathogens = {}
         self.ordered_snps = []
         self.snp_count = 0
         self.population_dir = OUTPUT_DIR
 
-    def generate_population(self, control_size, test_size, male_odds, snp_data):
+    def generate_population(self, control_size, test_size, male_odds, snp_data, pathogens_file):
         """Generate a simulated population based on the number of groups, mutations, size of test group,
         size of control group and the snp dictionary.
         1. Determine which snps will be the hidden pathogenic snps
@@ -80,10 +91,11 @@ class PopulationFactory:
         self.population_dir = OUTPUT_DIR + "/" + subdir + "/"
         os.makedirs(self.population_dir, exist_ok=True)
 
-        self.pick_pathogen_snps(snp_data)
         # Create map file
-
         self.output_map_file(snp_data)
+
+        self.pick_pathogen_snps(self.ordered_snps, pathogens_file)
+
         # Create control population
         self.output_population(control_size, True, male_odds)
         self.output_population(test_size, False, male_odds)
@@ -127,6 +139,14 @@ class PopulationFactory:
         :param male_odds: odds of a person being a biological male
         :return:
         """
+        if not is_control:
+            # pick pathogen groups for population size
+            pathogen_group_list = numpy.random.choices(
+                population=self.pathogens.values(),
+                weights=list(map(lambda x: x.population_weight), self.pathogens.values()),
+                k=size
+            )
+
         with open(self.population_dir + "population.ped", 'a+') as f:
             for i in range(size):
                 # Roll the dice for each snp and each allele. This will be a bit long for boys, but will work
@@ -135,6 +155,8 @@ class PopulationFactory:
                 snp_values = []
                 i = 0
                 row = 1
+                # If in test group... Select a pathogen group, then select pathogen snps.
+                pathogen_snps = pathogen_group_list[i].select_mutations
                 for chromo, snps in self.ordered_snps:
                     if not is_male and chromo == 'Y':
                         continue  # Skip Y snps for women
@@ -152,6 +174,7 @@ class PopulationFactory:
                             snp_values.append(selected_nt)
                             snp_values.append(other_nt)
                         else:
+                            # TODO incorporate new PathogenGroups code to select which snps are active for this person
                             selected_nt = snp.pick_pathogen_value()
                             snp_values.append(selected_nt)
                             snp_values.append(selected_nt)
@@ -168,24 +191,95 @@ class PopulationFactory:
                 f.write("\t".join(map(lambda x: str(x), [row, row, 0, 0, sex, affection])) + "\t" + "\t".join(snp_values) + "\n")
                 row += 1
 
-    def pick_pathogen_snps(self, snp_data):
+    def pick_pathogen_snps(self, snp_data, pathogens_config):
         """
         Pick and store the snps that are the pathogens. Randomly? pick num_mutations from the snps
-        :param snp_data: SNP candidates
-        :return:
+        :param pathogens_config: file path for pathogens yaml file
+        :param snp_data: SNPTuples which are candidates for being a pathogen
+        :return: nothing... self.pathogens is populated
         """
-        # Kinda inefficient... Could just pick based on total size and use indexes, but that is more complex
-        snp_id_list = []
-        for chromo, snps in snp_data.items():
-            snp_id_list.extend(list(map(lambda x: x.id, snps.values())))
-        for snp_id in numpy.random.choice(snp_id_list,  self.num_mutations):
-            self.pathogens[snp_id] = snp_id
+
+        with open(pathogens_config, 'r') as p:
+            pathogen_yml = yaml.load(p, Loader=yaml.FullLoader)
+            for group, group_attr in pathogen_yml.items():
+                iterations = 1
+                if group_attr['num_instances']:
+                    iterations = int(group_attr['num_instances'])
+                for i in range(0, iterations):
+                    path_group = PathogenGroup.from_yml(group_attr, snp_data)
+                    self.pathogens[group + "-" + i] = path_group
         with open(self.population_dir + "pathogens.txt", 'w') as f:
-            for pathogen_id in self.pathogens:
-                f.write(str(pathogen_id) + "\n")
+            for group_name, pathogen_group in self.pathogens.items():
+                f.write(str(group_name) + ":\n")
+                for snp_id, weight in pathogen_group.pathogens.items():
+                    f.write("%s\t%s\n" % (snp_id, weight))
 
     def is_pathogen(self, snp_id):
         return snp_id in self.pathogens
+
+
+class PathogenGroup:
+
+    def __init__(self, mutation_weights, snp_data, population_weight,
+                 min_minor_allele_freq=0, max_minor_allele_freq=1.1):
+        """
+        Inits the pathogen dictionary to be random alleles matching the freq filters. pathogens dict
+        stores snp.id => mutation weight mapping.
+        :param mutation_weights: list of floats of the value each picked
+        :param snp_data: snp dictionary
+        :param population_weight: the weight this pathogen group has (shares in test population)
+        """
+        self.pathogens = {}
+        self.population_weight = population_weight
+        snp_id_list = []
+        filter_snps = min_minor_allele_freq > 0 or max_minor_allele_freq < 0.5
+        for chromo, snps in snp_data:
+            filtered_list = snps
+            if filter_snps:
+                filtered_list = filter(
+                    lambda x: min_minor_allele_freq <= x.minor_allele_tuple()[1] <= max_minor_allele_freq,
+                    filtered_list)
+            snp_id_list.extend(list(map(lambda x: x.id, filtered_list)))
+        i = 0
+        for snp_id in numpy.random.choice(a=snp_id_list, size=len(mutation_weights), replace=False):
+            self.pathogens[snp_id] = mutation_weights[i]
+            i += 1
+
+    @classmethod
+    def from_yml(cls, yml_attr, snp_data):
+        min_minor_allele_freq = 0
+        max_minor_allele_freq = 1
+        if yml_attr.get('min_minor_allele_freq'):
+            if 0 < yml_attr['min_minor_allele_freq'] < 0.5:
+                min_minor_allele_freq = yml_attr['min_minor_allele_freq']
+            else:
+                raise Exception('min_minor_allele_freq must be between 0 and 0.5. yml value = {}'.format(
+                    yml_attr['min_minor_allele_freq']))
+        if yml_attr.get('max_minor_allele_freq'):
+            if 0 < yml_attr['max_minor_allele_freq'] < 0.5:
+                max_minor_allele_freq = yml_attr['max_minor_allele_freq']
+            else:
+                raise Exception('max_minor_allele_freq must be between 0 and 0.5. yml value = {}'.format(
+                    yml_attr['max_minor_allele_freq']))
+
+        return cls(yml_attr['mutation_weights'], snp_data, yml_attr['population_weight'],
+                   min_minor_allele_freq, max_minor_allele_freq)
+
+    def select_mutations(self):
+        """
+        Randomly select mutations a single individual might have if they are in this PathogenGroup
+        :return: a colleciton of snp_ids that are mutated
+        """
+        selected_pathogens = {}
+        shuffled_pathogens = list(self.pathogens.items())
+        random.shuffle(shuffled_pathogens) #Shuffle to randomly select
+        agg_weight = 0
+        for p in shuffled_pathogens:
+            selected_pathogens[p[0]] = p[1]
+            agg_weight += p[1] # sum the weights
+            if agg_weight >= 1:
+                break
+        return selected_pathogens
 
 
 def load_snps(dir, min_freq):
@@ -248,8 +342,6 @@ def load_snps(dir, min_freq):
 def print_help():
     print("""
     Accepted Inputs are:
-    -g number of hidden subgroups all with same phenotype
-    -n total number of different mutations that can cause a subphenotype (min 1 per subgroup)
     -s size of test group (afflicted group)
     -c size of control group
     -f min frequency for a SNP to be included in the list of SNPs, default is 0.005
@@ -258,7 +350,7 @@ def print_help():
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "h?ngfs:c:t", ["help"])
+        opts, args = getopt.getopt(argv, "h?pfs:c:t", ["help"])
     except getopt.GetoptError as err:
         print(err.msg)
         print_help()
@@ -267,14 +359,13 @@ def main(argv):
     num_mutations = 1
     min_freq = MIN_SNP_FREQ
     male_odds = 0.5
+    pathogens_file = 'pathogens.yml'
     for opt, arg in opts:
         if opt in ('-h', "-?", "--help"):
             print_help()
             sys.exit()
-        elif opt in "-n":
-            num_mutations = int(arg)
-        elif opt in "-g":
-            subgroups = int(arg)
+        elif opt in "-p":
+            pathogens_file = arg
         elif opt in "-s":
             size = int(arg)
         elif opt in "-c":
@@ -286,8 +377,8 @@ def main(argv):
         elif opt in "-t":
             TEST_MODE = True
     snps = load_snps(SNP_DIR, min_freq)
-    pop_factory = PopulationFactory(subgroups, num_mutations)
-    pop_factory.generate_population(control_size, size, male_odds, snps)
+    pop_factory = PopulationFactory()
+    pop_factory.generate_population(control_size, size, male_odds, snps, pathogens_file)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- test/pop_factory_test.py
+++ test/pop_factory_test.py
@@ -1,6 +1,13 @@
 import unittest
 import pop_factory
 
+def test_snp_tuple(snp_id):
+    snp_tuple = pop_factory.SNPTuples(snp_id)
+    snp_tuple.add_tuple("G", 0.20)
+    snp_tuple.add_tuple("A", 0.70)
+    snp_tuple.add_tuple("T", 1.0)
+    return snp_tuple
+
 class PopulationFactoryTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
@@ -9,10 +16,7 @@ class PopulationFactoryTest(unittest.TestCase):
 class SNPTupleTest(unittest.TestCase):
 
     def setUp(self):
-        self.snp_tuple = pop_factory.SNPTuples(100)
-        self.snp_tuple.add_tuple("G", 0.20)
-        self.snp_tuple.add_tuple("A", 0.70)
-        self.snp_tuple.add_tuple("T", 1.0)
+        self.snp_tuple = test_snp_tuple(100)
 
     def test_pick_nt(self):
         picked = self.snp_tuple.pick_snp_value(0.95)
@@ -24,6 +28,19 @@ class SNPTupleTest(unittest.TestCase):
         picked = self.snp_tuple.pick_pathogen_value()
         self.assertEqual("T", picked, "Should pick T as pathogen (2nd most frequent)")
 
+class PathogenGroupTest(unittest.TestCase):
+
+    def setUp(self):
+        self.snp_data = [(1, [test_snp_tuple(1), test_snp_tuple(2), test_snp_tuple(3), test_snp_tuple(4)])]
+
+    def test_from_yml(self):
+        pg = pop_factory.PathogenGroup.from_yml({"mutation_weights": [0.5, 0.5, 0.5],
+                                                 "num_instances": 2,
+                                                 "population_weight": 5,
+                                                 "min_minor_allele_freq": 0.01}, self.snp_data)
+        self.assertEqual(5, pg.population_weight, "Population Weight should be 5")
+        self.assertEqual(3, len(pg.pathogens), "Should have picked 3 pathogens")
+        self.assertEqual(2, len(pg.select_mutations()))
 
 if __name__ == '__main__':
     unittest.main()
